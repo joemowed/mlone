8de62a0ee4b444f4b2ab48fa9295a941ebd42709
joemowed <maloney.jg2@gmail.com>
2023-12-06 00:45:37 -0500

    testgit

--- src/include/matrix.hpp
+++ src/include/matrix.hpp
@@ -1,26 +1,25 @@
-#include <array>
+#include <algorithm>
 #include <cassert>
+#include <chrono>
 #include <cstddef>
 #include <iostream>
 #include <memory>
-#include <stdatomic.h>
+#include <thread>
 #include <vector>
+
 template <std::size_t rowSize, std::size_t colSize> class matrix {
 private:
-  std::vector<float> matrixData;
   std::size_t flattenIndex(const std::size_t &rowIndex,
                            const std::size_t &colIndex) {
-    // std::cout << "rowIndex is " << rowIndex << " , " << colIndex <<
-    // std::endl; std::cout << "size is " << rowSize << " , " << colSize <<
-    // std::endl;
     assert(rowIndex < colSize);
     assert(colIndex < rowSize);
     return rowIndex * rowSize + colIndex;
   }
 
 public:
+  std::vector<float> matrixData;
   matrix() { matrixData.resize(rowSize * colSize, 0); };
-
+  matrix(const matrix<rowSize, colSize> &t) { matrixData = t.matrixData; }
   void setValue(const std::size_t &rowIndex, const std::size_t &colIndex,
                 const float &value) {
     matrixData.at(flattenIndex(rowIndex, colIndex)) = value;
@@ -41,10 +40,127 @@ public:
   }
 };
 namespace MOP {
+
+const std::size_t MULTIPLY_TRANS_OPTIMIZATION_SIZE = 1;
+const std::size_t MULTIPLY_TRANS_THREADS = 2;
+template <std::size_t originRowSize, std::size_t originColSize>
+void transpose(
+    const std::unique_ptr<matrix<originRowSize, originColSize>> &originalMat,
+    const std::unique_ptr<matrix<originColSize, originRowSize>> &transMat) {
+  for (std::size_t originRowIndex = 0; originRowIndex < originColSize;
+       originRowIndex++) {
+    for (std::size_t originColIndex = 0; originColIndex < originRowSize;
+         originColIndex++) {
+      transMat->setValue(originColIndex, originRowIndex,
+                         originalMat->getValue(originRowIndex, originColIndex));
+    }
+  }
+}
+template <std::size_t rowSize, std::size_t colSize, std::size_t aColSize,
+          std::size_t bColSize>
+void concatForMult(
+    const std::unique_ptr<matrix<rowSize, aColSize>> &A,
+    const std::unique_ptr<matrix<rowSize, bColSize>> &B,
+    const std::unique_ptr<matrix<rowSize, (aColSize + bColSize)>> &dest) {
+  dest->matrixData = A->matrixData;
+  dest->matrixData.insert(dest->matrixData.end(), B->matrixData.begin(),
+                          B->matrixData.end());
+}
+// 0 is the first slice, the source and destinsation matrix must have the same
+// rowSize
+template <std::size_t aRowSize, std::size_t aColSize, std::size_t bColSize>
+void sliceHelper(const std::unique_ptr<matrix<aRowSize, aColSize>> &A,
+                 const std::unique_ptr<matrix<aRowSize, bColSize>> &dest,
+                 const std::size_t &slice) {
+  const std::size_t endOffset = aRowSize * bColSize;
+  const std::size_t sliceOffset = slice * endOffset;
+  dest->matrixData.assign(A->matrixData.begin() + sliceOffset,
+                          A->matrixData.begin() + sliceOffset + endOffset);
+}
+
+template <std::size_t bRowSize, std::size_t sharedSize, std::size_t aColSize>
+// needs B to be transposed
+void transMultThread(const std::unique_ptr<matrix<sharedSize, aColSize>> &A,
+                     const std::unique_ptr<matrix<sharedSize, bRowSize>> &B,
+                     const std::unique_ptr<matrix<bRowSize, aColSize>> &dest) {
+
+  for (std::size_t aCurrRow = 0; aCurrRow < aColSize; aCurrRow++) {
+    for (std::size_t bCurrCol = 0; bCurrCol < bRowSize; bCurrCol++) {
+      float sum = 0;
+      for (std::size_t dotIter = 0; dotIter < sharedSize; dotIter++) {
+        sum += A->getValue(aCurrRow, dotIter) * B->getValue(bCurrCol, dotIter);
+      }
+      dest->setValue(aCurrRow, bCurrCol, sum);
+    }
+  }
+}
+
+template <std::size_t bRowSize, std::size_t sharedSize, std::size_t aColSize>
+void transMult(const std::unique_ptr<matrix<sharedSize, aColSize>> &A,
+               const std::unique_ptr<matrix<bRowSize, sharedSize>> &B,
+               const std::unique_ptr<matrix<bRowSize, aColSize>> &dest) {
+
+  auto transMat = std::make_unique<matrix<sharedSize, bRowSize>>();
+  transpose<bRowSize, sharedSize>(B, transMat);
+  std::vector<std::unique_ptr<matrix<sharedSize, bRowSize>>> bMats;
+  constexpr const std::size_t batchColSize = aColSize / MULTIPLY_TRANS_THREADS;
+  constexpr const std::size_t lastColSize = aColSize % MULTIPLY_TRANS_THREADS;
+  std::vector<std::unique_ptr<matrix<sharedSize, batchColSize>>> aMats;
+  auto lastAMat = std::make_unique<matrix<sharedSize, lastColSize>>();
+  std::vector<std::unique_ptr<matrix<bRowSize, batchColSize>>> cMats;
+  auto lastCMat = std::make_unique<matrix<bRowSize, lastColSize>>();
+  constexpr const std::size_t lastAMatOffsetBegin =
+      batchColSize * sharedSize * MULTIPLY_TRANS_THREADS;
+  lastAMat->matrixData.assign(A->matrixData.begin() + lastAMatOffsetBegin,
+                              A->matrixData.end());
+  for (std::size_t constructItor = 0; constructItor < MULTIPLY_TRANS_THREADS;
+       constructItor++) {
+    aMats.push_back(std::make_unique<matrix<sharedSize, batchColSize>>());
+  };
+
+  ;
+  for (std::size_t constructItor = 0; constructItor <= MULTIPLY_TRANS_THREADS;
+       constructItor++) {
+    bMats.push_back(std::make_unique<matrix<sharedSize, bRowSize>>(*transMat));
+  }
+
+  for (std::size_t constructItor = 0; constructItor < MULTIPLY_TRANS_THREADS;
+       constructItor++) {
+    cMats.push_back(std::make_unique<matrix<bRowSize, batchColSize>>());
+  }
+
+  for (std::size_t genItor = 0; genItor < MULTIPLY_TRANS_THREADS; genItor++) {
+    sliceHelper<sharedSize, aColSize, batchColSize>(A, aMats.at(genItor),
+                                                    genItor);
+  }
+  for (std::size_t genItor = 0; genItor < MULTIPLY_TRANS_THREADS; genItor++) {
+    transMultThread<bRowSize, sharedSize, batchColSize>(
+        aMats.at(genItor), bMats.at(genItor), cMats.at(genItor));
+  }
+
+  transMultThread<bRowSize, sharedSize, lastColSize>(
+      lastAMat, bMats.at(MULTIPLY_TRANS_THREADS), lastCMat);
+
+  dest->matrixData = {};
+  for (std::size_t genItor = 0; genItor < MULTIPLY_TRANS_THREADS; genItor++) {
+    dest->matrixData.insert(dest->matrixData.end(),
+                            cMats.at(genItor)->matrixData.begin(),
+                            cMats.at(genItor)->matrixData.end());
+  }
+
+  dest->matrixData.insert(dest->matrixData.end(), lastCMat->matrixData.begin(),
+                          lastCMat->matrixData.end());
+}
 template <std::size_t bRowSize, std::size_t sharedSize, std::size_t aColSize>
 void multiply(const std::unique_ptr<matrix<sharedSize, aColSize>> &A,
               const std::unique_ptr<matrix<bRowSize, sharedSize>> &B,
               const std::unique_ptr<matrix<bRowSize, aColSize>> &dest) {
+  if (sharedSize >= MULTIPLY_TRANS_OPTIMIZATION_SIZE &&
+      bRowSize >= MULTIPLY_TRANS_OPTIMIZATION_SIZE &&
+      aColSize >= MULTIPLY_TRANS_OPTIMIZATION_SIZE) {
+    transMult<bRowSize, sharedSize, aColSize>(A, B, dest);
+    return;
+  }
   for (std::size_t aCurrRow = 0; aCurrRow < aColSize; aCurrRow++) {
     for (std::size_t bCurrCol = 0; bCurrCol < bRowSize; bCurrCol++) {
       float sum = 0;
--- src/testing/matrixTest.cc
+++ src/testing/matrixTest.cc
@@ -28,7 +28,7 @@ void matrixTestSetSingle(
 
     for (const float &each : eachRow) {
       thisMatrix->setValue(i, ii, each);
-      EXPECT_EQ(thisMatrix->getValue(i, ii), each);
+      ASSERT_EQ(thisMatrix->getValue(i, ii), each);
       ii++;
     }
     ii = 0;
@@ -42,15 +42,15 @@ void matrixTestInitZero(
   for (std::size_t i = 0; i < colSize; i++) {
 
     for (std::size_t ii = 0; ii < rowSize; ii++) {
-      EXPECT_EQ(thisMatrix->getValue(i, ii), 0);
+      ASSERT_EQ(thisMatrix->getValue(i, ii), 0);
     }
   }
 }
 TEST(testTest, testTest) {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "heelo");
+  ASSERT_STRNE("hello", "heelo");
   // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  ASSERT_EQ(7 * 6, 42);
 }
 TEST(matrixTest, matrixSetAndGet) {
   auto squareMatrix = std::make_unique<matrix<5, 5>>();
@@ -58,54 +58,90 @@ TEST(matrixTest, matrixSetAndGet) {
   auto colMatrix = std::make_unique<matrix<5, 10>>();
   auto oneRowMatrix = std::make_unique<matrix<20, 1>>();
   auto oneColMatrix = std::make_unique<matrix<1, 20>>();
-  auto bigMatrix = std::make_unique<matrix<1000, 1000>>();
+  auto bigMatrix = std::make_unique<matrix<500, 500>>();
   std::array<std::array<float, 5>, 5> squareTest;
   std::array<std::array<float, 5>, 10> colTest;
   std::array<std::array<float, 10>, 5> rowTest;
   std::array<std::array<float, 20>, 1> oneRowTest;
   std::array<std::array<float, 1>, 20> oneColTest;
-  std::array<std::array<float, 1000>, 1000> bigTest;
+  std::array<std::array<float, 500>, 500> bigTest;
   matrixTestArrayFill<5, 5>(squareTest);
   matrixTestArrayFill<5, 10>(colTest);
   matrixTestArrayFill<10, 5>(rowTest);
   matrixTestArrayFill<1, 20>(oneColTest);
   matrixTestArrayFill<20, 1>(oneRowTest);
-  matrixTestArrayFill<1000, 1000>(bigTest);
+  matrixTestArrayFill<500, 500>(bigTest);
   matrixTestSetSingle<5, 10>(colTest, colMatrix.get());
   matrixTestSetSingle<5, 5>(squareTest, squareMatrix.get());
   matrixTestSetSingle<10, 5>(rowTest, rowMatrix.get());
   matrixTestSetSingle<1, 20>(oneColTest, oneColMatrix.get());
   matrixTestSetSingle<20, 1>(oneRowTest, oneRowMatrix.get());
-  matrixTestSetSingle<1000, 1000>(bigTest, bigMatrix.get());
+  matrixTestSetSingle<500, 500>(bigTest, bigMatrix.get());
 }
+TEST(matrixTest, copyAndTranpose) {
+  auto squareMatrix = std::make_unique<matrix<5, 5>>();
+  auto rowMatrix = std::make_unique<matrix<10, 5>>();
+  auto colMatrix = std::make_unique<matrix<5, 10>>();
+  auto oneRowMatrix = std::make_unique<matrix<20, 1>>();
+  auto oneColMatrix = std::make_unique<matrix<1, 20>>();
+  auto bigMatrix = std::make_unique<matrix<500, 500>>();
+  std::array<std::array<float, 5>, 5> squareTest;
+  std::array<std::array<float, 5>, 10> colTest;
+  std::array<std::array<float, 10>, 5> rowTest;
+  std::array<std::array<float, 20>, 1> oneRowTest;
+  std::array<std::array<float, 1>, 20> oneColTest;
+  std::array<std::array<float, 500>, 500> bigTest;
+  matrixTestArrayFill<5, 5>(squareTest);
+  matrixTestArrayFill<5, 10>(colTest);
+  matrixTestArrayFill<10, 5>(rowTest);
+  matrixTestArrayFill<1, 20>(oneColTest);
+  matrixTestArrayFill<20, 1>(oneRowTest);
+  matrixTestArrayFill<500, 500>(bigTest);
+  matrixTestSetSingle<5, 10>(colTest, colMatrix.get());
+  matrixTestSetSingle<5, 5>(squareTest, squareMatrix.get());
+  matrixTestSetSingle<10, 5>(rowTest, rowMatrix.get());
+  matrixTestSetSingle<1, 20>(oneColTest, oneColMatrix.get());
+  matrixTestSetSingle<20, 1>(oneRowTest, oneRowMatrix.get());
+  matrixTestSetSingle<500, 500>(bigTest, bigMatrix.get());
+  auto copy1 = std::make_unique<matrix<5, 10>>();
+  auto copy3 = std::make_unique<matrix<10, 5>>(*rowMatrix);
+  ASSERT_EQ(copy3->matrixData, rowMatrix->matrixData);
+  MOP::transpose<10, 5>(rowMatrix, copy1);
+  ASSERT_NE(copy1->matrixData, rowMatrix->matrixData);
+  MOP::transpose<5, 10>(copy1, copy3);
 
+  ASSERT_EQ(copy3->matrixData, rowMatrix->matrixData);
+}
 TEST(matrixTest, matrixInitZero) {
   auto squareMatrix = std::make_unique<matrix<5, 5>>();
   auto rowMatrix = std::make_unique<matrix<10, 5>>();
   auto colMatrix = std::make_unique<matrix<5, 10>>();
   auto oneRowMatrix = std::make_unique<matrix<20, 1>>();
   auto oneColMatrix = std::make_unique<matrix<1, 20>>();
-  auto bigMatrix = std::make_unique<matrix<1000, 1000>>();
+  auto bigMatrix = std::make_unique<matrix<500, 500>>();
   std::array<std::array<float, 5>, 5> squareTest;
   std::array<std::array<float, 5>, 10> colTest;
   std::array<std::array<float, 10>, 5> rowTest;
   std::array<std::array<float, 20>, 1> oneRowTest;
   std::array<std::array<float, 1>, 20> oneColTest;
-  std::array<std::array<float, 1000>, 1000> bigTest;
+  std::array<std::array<float, 500>, 500> bigTest;
   matrixTestArrayFill<5, 5>(squareTest);
   matrixTestArrayFill<5, 10>(colTest);
   matrixTestArrayFill<10, 5>(rowTest);
   matrixTestArrayFill<1, 20>(oneColTest);
   matrixTestArrayFill<20, 1>(oneRowTest);
-  matrixTestArrayFill<1000, 1000>(bigTest);
+  matrixTestArrayFill<500, 500>(bigTest);
   matrixTestInitZero<5, 10>(colTest, colMatrix.get());
   matrixTestInitZero<5, 5>(squareTest, squareMatrix.get());
   matrixTestInitZero<10, 5>(rowTest, rowMatrix.get());
   matrixTestInitZero<1, 20>(oneColTest, oneColMatrix.get());
   matrixTestInitZero<20, 1>(oneRowTest, oneRowMatrix.get());
-  matrixTestInitZero<1000, 1000>(bigTest, bigMatrix.get());
+  matrixTestInitZero<500, 500>(bigTest, bigMatrix.get());
+  std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\ntesting a1,b1\n";
 }
 TEST(matrixTest, matrixMultTest) {
+
+  std::cout << "\ntesting a1,b1\n";
   auto A1 = std::make_unique<matrix<3, 3>>();
   auto B1 = std::make_unique<matrix<1, 3>>();
   auto C1 = std::make_unique<matrix<1, 3>>();
@@ -137,18 +173,124 @@ TEST(matrixTest, matrixMultTest) {
   B2->setValue(0, 1, 4);
   B2->setValue(1, 1, 5);
   B2->setValue(2, 1, 6);
+  std::cout << "\ntesting a1,b1\n";
   MOP::multiply<1, 3, 3>(A1, B1, C1);
+  std::cout << "\ntesting a1,b2\n";
   MOP::multiply<2, 3, 3>(A1, B2, C2);
+  std::cout << "\ntesting a3,b3\n";
   MOP::multiply<500, 500, 500>(A2, B3, C3);
-  EXPECT_EQ(C1->getValue(0, 0), 14);
-  EXPECT_EQ(C1->getValue(1, 0), 32);
-  EXPECT_EQ(C1->getValue(2, 0), 50);
-  EXPECT_EQ(C2->getValue(0, 0), 14);
-  EXPECT_EQ(C2->getValue(1, 0), 32);
-  EXPECT_EQ(C2->getValue(2, 0), 50);
+  ASSERT_EQ(C1->getValue(0, 0), 14);
+  ASSERT_EQ(C1->getValue(1, 0), 32);
+  ASSERT_EQ(C1->getValue(2, 0), 50);
+  ASSERT_EQ(C2->getValue(0, 0), 14);
+  ASSERT_EQ(C2->getValue(1, 0), 32);
+  ASSERT_EQ(C2->getValue(2, 0), 50);
 
-  EXPECT_EQ(C2->getValue(0, 1), 32);
-  EXPECT_EQ(C2->getValue(1, 1), 77);
-  EXPECT_EQ(C2->getValue(2, 1), 122);
+  ASSERT_EQ(C2->getValue(0, 1), 32);
+  ASSERT_EQ(C2->getValue(1, 1), 77);
+  ASSERT_EQ(C2->getValue(2, 1), 122);
+}
+TEST(matrixTest, concatMat) {
+  auto squareMatrix = std::make_unique<matrix<5, 5>>();
+  auto rowMatrix = std::make_unique<matrix<10, 5>>();
+  auto colMatrix = std::make_unique<matrix<5, 10>>();
+  auto oneRowMatrix = std::make_unique<matrix<20, 1>>();
+  auto oneColMatrix = std::make_unique<matrix<1, 20>>();
+  auto bigMatrix = std::make_unique<matrix<500, 500>>();
+  std::array<std::array<float, 5>, 5> squareTest;
+  std::array<std::array<float, 5>, 10> colTest;
+  std::array<std::array<float, 10>, 5> rowTest;
+  std::array<std::array<float, 20>, 1> oneRowTest;
+  std::array<std::array<float, 1>, 20> oneColTest;
+  std::array<std::array<float, 500>, 500> bigTest;
+  matrixTestArrayFill<5, 5>(squareTest);
+  matrixTestArrayFill<5, 10>(colTest);
+  matrixTestArrayFill<10, 5>(rowTest);
+  matrixTestArrayFill<1, 20>(oneColTest);
+  matrixTestArrayFill<20, 1>(oneRowTest);
+  matrixTestArrayFill<500, 500>(bigTest);
+  matrixTestSetSingle<5, 10>(colTest, colMatrix.get());
+  matrixTestSetSingle<5, 5>(squareTest, squareMatrix.get());
+  matrixTestSetSingle<10, 5>(rowTest, rowMatrix.get());
+  matrixTestSetSingle<1, 20>(oneColTest, oneColMatrix.get());
+  matrixTestSetSingle<20, 1>(oneRowTest, oneRowMatrix.get());
+  matrixTestSetSingle<500, 500>(bigTest, bigMatrix.get());
+  auto first = std::make_unique<matrix<2, 3>>();
+  auto second = std::make_unique<matrix<2, 3>>();
+  auto result = std::make_unique<matrix<2, 6>>();
+  auto compare = std::make_unique<matrix<2, 6>>();
+  float i = 0;
+  for (float &each : first->matrixData) {
+    each = i;
+    i++;
+  }
+  i = 0;
+  for (float &each : second->matrixData) {
+    each = i;
+    i++;
+  }
+  i = 0;
+  for (float &each : compare->matrixData) {
+    i = (i >= 6) ? 0 : i;
+    each = i;
+    i++;
+  }
+
+  MOP::concatForMult<2, 6, 3, 3>(first, second, result);
+  ASSERT_EQ(compare->matrixData, result->matrixData);
+}
+TEST(matrixTest, sliceMat) {
+  auto squareMatrix = std::make_unique<matrix<5, 5>>();
+  auto rowMatrix = std::make_unique<matrix<10, 5>>();
+  auto colMatrix = std::make_unique<matrix<5, 10>>();
+  auto oneRowMatrix = std::make_unique<matrix<20, 1>>();
+  auto oneColMatrix = std::make_unique<matrix<1, 20>>();
+  auto bigMatrix = std::make_unique<matrix<500, 500>>();
+  std::array<std::array<float, 5>, 5> squareTest;
+  std::array<std::array<float, 5>, 10> colTest;
+  std::array<std::array<float, 10>, 5> rowTest;
+  std::array<std::array<float, 20>, 1> oneRowTest;
+  std::array<std::array<float, 1>, 20> oneColTest;
+  std::array<std::array<float, 500>, 500> bigTest;
+  matrixTestArrayFill<5, 5>(squareTest);
+  matrixTestArrayFill<5, 10>(colTest);
+  matrixTestArrayFill<10, 5>(rowTest);
+  matrixTestArrayFill<1, 20>(oneColTest);
+  matrixTestArrayFill<20, 1>(oneRowTest);
+  matrixTestArrayFill<500, 500>(bigTest);
+  matrixTestSetSingle<5, 10>(colTest, colMatrix.get());
+  matrixTestSetSingle<5, 5>(squareTest, squareMatrix.get());
+  matrixTestSetSingle<10, 5>(rowTest, rowMatrix.get());
+  matrixTestSetSingle<1, 20>(oneColTest, oneColMatrix.get());
+  matrixTestSetSingle<20, 1>(oneRowTest, oneRowMatrix.get());
+  matrixTestSetSingle<500, 500>(bigTest, bigMatrix.get());
+  auto first = std::make_unique<matrix<2, 3>>();
+  auto second = std::make_unique<matrix<2, 3>>();
+  auto result = std::make_unique<matrix<2, 6>>();
+
+  auto compare = std::make_unique<matrix<2, 6>>();
+  float i = 0;
+  for (float &each : first->matrixData) {
+    each = i;
+    i++;
+  }
+  i = 0;
+  for (float &each : second->matrixData) {
+    each = i;
+    i++;
+  }
+  i = 0;
+  for (float &each : compare->matrixData) {
+    i = (i >= 6) ? 0 : i;
+    each = i;
+    i++;
+  }
+  auto slice1 = std::make_unique<matrix<2, 3>>();
+  auto slice2 = std::make_unique<matrix<2, 3>>();
+  MOP::concatForMult<2, 6, 3, 3>(first, second, result);
+  MOP::sliceHelper<2, 6, 3>(result, slice1, 0);
+  MOP::sliceHelper<2, 6, 3>(result, slice2, 1);
+  ASSERT_EQ(first->matrixData, slice1->matrixData);
+  ASSERT_EQ(second->matrixData, slice2->matrixData);
 }
 #endif // !MATRIX_TEST
