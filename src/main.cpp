307411527c3f24812f715c6cc91ce4a05860312e
joemowed <maloney.jg2@gmail.com>
2023-12-07 18:11:32 -0500

    MULTITHREADING BABY

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "benchmark.hpp"
 #include "fileio.hpp"
 #include "lodepng.h"
 #include <iomanip>
@@ -5,16 +6,22 @@
 #include <memory>
 #include <string>
 int main() {
+  benchMark tot("main");
   std::cout << "running mlone.exe\n";
   auto load = std::make_unique<loader>(FIO::MNIST_ROOT_PATH_TRAINING);
+  benchMark benchgfn("get file names");
   load->generateFileNames();
   for (auto &each : load->catagories) {
     std::cout << each << std::endl;
   }
+  benchgfn.stop();
+  benchMark benchlf("load files");
   load->loadDataSet();
   FIO::displayMatrixAsImage(load->loadedImages.at(58949));
   FIO::displayMatrixAsImage(load->loadedImages.at(40398));
-  FIO::displayMatrixAsImage(load->loadedImages.at(3888));
-
+  FIO::displayMatrixAsImage(load->loadedImages.at(20042));
+  benchlf.stop();
+  tot.stop();
+  std::cout << load->loadedImages.size();
   return 0;
 }
