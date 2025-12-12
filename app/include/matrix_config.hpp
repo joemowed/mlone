db50d34720e7cf5ac5fa8e1b0168ac8c6515d233
Joe Maloney <maloney.jg2@gmail.com>
2025-12-12 11:50:26 -0500

    Change to preprocessor defined polymorphism

--- app/include/matrix_config.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __MATRIX_CONFIG_INC_GAURD__
-#define __MATRIX_CONFIG_INC_GAURD__
-
-/* Underlying datatype of all matrix instances.  Change this here to suit
- * the application */
-using Base_t = float;
-#endif
